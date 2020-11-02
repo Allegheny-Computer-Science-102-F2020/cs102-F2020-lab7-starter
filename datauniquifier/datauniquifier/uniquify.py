ac80f3a7137b849c080a085a37eb0d90ff35ffce
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-02 14:47:20 -0500

    Add the uniquify module with the required TODO markers.

---
+++ datauniquifier/datauniquifier/uniquify.py
@@ -0,0 +1,48 @@
+"""Perform uniquification on the provided input string."""
+
+from typing import List
+
+from functools import wraps
+from time import time
+
+
+def timing(function):
+    """Define a profiling function for execution time."""
+    # Reference:
+    # https://stackoverflow.com/questions/1622943/timeit-versus-timing-decorator
+    @wraps(function)
+    def wrap(*args, **kw):
+        ts = time()
+        result = function(*args, **kw)
+        te = time()
+        print("The function %r took: %2.4f sec" % (function.__name__, te - ts))
+        return result
+
+    return wrap
+
+
+# TODO: Read the reference for a description of the functions:
+#
+# https://www.peterbe.com/plog/fastest-way-to-uniquify-a-list-in-python-3.6
+#
+# Note that this module does not contain all of the functions in the post
+
+# TODO: If you would like to do so, add and then experimentally evaluate more methods
+
+
+@timing
+def unique_set(data: List[str]) -> List[str]:
+    """Perform uniquification of the input list of strings and return results in a list of strings."""
+    # TODO: Add the source code for method f7
+
+
+@timing
+def unique_setcomprehension(data: List[str]) -> List[str]:
+    """Perform uniquification of the input list of strings and return results in a list of strings."""
+    # TODO: Add the source code for method f8
+
+
+@timing
+def unique_dictionary(data: List[str]) -> List[str]:
+    """Perform uniquification of the input list of strings and return results in a list of strings."""
+    # TODO: Add the source code for method f12
